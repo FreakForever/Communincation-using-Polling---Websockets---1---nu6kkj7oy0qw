9308c60062004e9449af2e447f11111c1a3d74c4
FreakForever <147957109+FreakForever@users.noreply.github.com>
2024-12-01 02:28:12 +0530

    my first commit.

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ app.use(express.json());
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
-
+app.post('/message', (req, res) =>{
+  const {text, user} = req.body;
+  if(!text || !user){
+    res.status(400).json({error : "Please provide a valid input"})
+  }
+  try{
+    const newMessage = {
+      user, 
+      text,
+      timestamp : new Date().toISOString(),
+    }
+    messages.push(newMessage)
+    res.status(200).json({message : newMessage})
+  }
+  catch(error){
+    res.status(500).json({error : "Internal server error."})
+  }
+})
 const messages = [];
 
 app.listen(port, () => {
