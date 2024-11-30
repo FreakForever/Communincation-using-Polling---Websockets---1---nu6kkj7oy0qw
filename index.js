b023b4741a653245ef0aaec3055cf6e7eb31f608
FreakForever <147957109+FreakForever@users.noreply.github.com>
2024-12-01 02:47:58 +0530

    my first commit.

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+
 app.use(express.json());
+
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 const messages = [];
-app.get("/findMessages",(req,res)=>{
-  res.status(200).json({messages})
+app.get('/findMessages', (req, res)=>{
+  return res.status(200).json({messages})
 })
-app.post("/message",(req,res)=>{
-  const {text,user}=req.body;
-  if(!text||!user){
-    return res.status(400).json({ error: "Please provide a valid input" });
+app.post('/message', (req, res) =>{
+  const {text, user} = req.body;
+  if(!text || !user){
+    return res.status(400).json({error :"Please provide a valid input"})
   }
-  const newMessage={
-    user,
-    text,
-    timestamp:new Date().toISOString(),
-  }
-  messages.push(newMessage);
-  res.status(200).json({message:newMessage});
+    const newMessage = {
+      user, 
+      text,
+      timestamp : new Date().toISOString(),
+    }
+    messages.push(newMessage)
+    res.status(200).json({message : newMessage})
+
 })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
 module.exports = { app, messages };
