2cca311452f3aa8e26733853f3e6ba270300db52
FreakForever <147957109+FreakForever@users.noreply.github.com>
2024-12-01 02:45:30 +0530

    my first commit.

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-
 app.use(express.json());
-
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
-app.get('/findMessage', (req, res)=>{
-  return res.status(200).json({messages})
-})
-app.post('/message', (req, res) =>{
-  const {text, user} = req.body;
-  if(!text || !user){
-    return res.status(400).json({error : "Please provide a valid input"})
-  }
-    const newMessage = {
-      user, 
-      text,
-      timestamp : new Date().toISOString(),
-    }
-    messages.push(newMessage)
-    return res.status(200).json({message : newMessage})
-
-})
 const messages = [];
-
+app.get("/findMessages",(req,res)=>{
+  res.status(200).json({messages})
+})
+app.post("/message",(req,res)=>{
+  const {text,user}=req.body;
+  if(!text||!user){
+    return res.status(400).json({ error: "Please provide a valid input" });
+  }
+  const newMessage={
+    user,
+    text,
+    timestamp:new Date().toISOString(),
+  }
+  messages.push(newMessage);
+  res.status(200).json({message:newMessage});
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
 module.exports = { app, messages };
